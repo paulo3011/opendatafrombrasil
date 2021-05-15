ce0460e1c7a8df07549fdae4a4a2f3f9304ee20d
Paulo Moreira <paulo3011@hotmail.com>
2021-05-15 12:40:56 -0300

    chore: Adds initial Spark app template

--- source/java/opendata_etl/src/main/java/application/batch/App.java
+++ source/java/opendata_etl/src/main/java/application/batch/App.java
@@ -1,7 +1,23 @@
 package application.batch;
 
+import application.batch.models.Parameters;
+import org.apache.spark.SparkConf;
+import org.apache.spark.sql.SparkSession;
+
 public class App {
     public static void main(String[] args) {
         System.out.println("Starting app program..");
+        Parameters parameters = new Parameters(args);
+
+        System.out.println("Starting SparkSession..");
+        SparkConf sparkConf = new SparkConf()
+                .setMaster("local[*]");
+
+        SparkSession sparkSession = SparkSession
+                .builder()
+                .config(sparkConf)
+                .getOrCreate();
+
+        System.out.println("Ended SparkSession..");
     }
 }
--- source/java/opendata_etl/src/main/java/application/batch/models/Parameters.java
+++ source/java/opendata_etl/src/main/java/application/batch/models/Parameters.java
@@ -1,7 +1,21 @@
 package application.batch.models;
 
+import lombok.Getter;
+import lombok.Setter;
+import com.beust.jcommander.Parameter;
+
 /**
  * Batch Application parameters
  */
 public class Parameters {
+    @Getter
+    @Setter
+    @Parameter
+    private String outputPath;
+
+    public Parameters(){}
+    public Parameters(String[] argv){
+        //this.parse(argv);
+        System.out.println("Parameters not implemented yet");
+    }
 }
